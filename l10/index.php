0838794d63e31df02de40b3ea878e0f44e8139fc
Vladimir <adfix@bk.ru>
2016-05-21 20:24:37 +0400

    Pravka papok

--- l10/index.php
+++ l10/index.php
@@ -1,28 +1,6 @@
 <?php
+require_once __DIR__.'/model/news.php';
 
-abstract class Article {
-    public $title;
-    public $text;
-    static protected $delim=': ';
-    public function __construct($title, $text='') {
-        $this->title =$title;
-        $this->text =$text;
+$items=news::getAll();
 
-    }
-
-    public function view() {
-        echo $this->title;
-    }
-}
-
-class NewsArticle extends Article {
-    public $author;
-    public function view() {
-        echo $this->author . self::$delim. $this->title;
-    }
-}
-
-$a=new NewsArticle("Привет!");
-$a->author="Автор";
-$a->view();
 ?>
